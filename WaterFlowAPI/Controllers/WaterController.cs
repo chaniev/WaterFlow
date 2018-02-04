5fbe6d5380c2665b46bde23cd3ff5d8a46d88efc
chaniev <chaniev@gmail.com>
2018-02-04 16:42:17 +0300

    rename

--- WaterFlowAPI/Controllers/WaterController.cs
+++ WaterFlowAPI/Controllers/WaterController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WaterFlowAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class WaterController : Controller
     {
         // GET api/values
         [HttpGet]
