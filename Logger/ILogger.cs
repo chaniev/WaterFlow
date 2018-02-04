5c89c826e3aaf5585a57fff28ecb05f95996b645
chaniev <chaniev@gmail.com>
2018-02-04 16:57:13 +0300

    проект для логгирования

---
+++ Logger/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Logger
+{
+    public interface ILogger
+    {
+        void Debug(string _className, string _message);
+        void Error(string _className, string _message);
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Logger
+{
+    public class Logger : ILogger
+    {
+        public void Debug(string _className, string _message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string _className, string _message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
